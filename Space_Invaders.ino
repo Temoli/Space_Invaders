576e166783bc5b51d0a05403d81ff8fa58951685
Temoli <ari.mokori@gmail.com>
2021-04-26 20:01:25 +0200

    Update Space_Invaders.ino

--- Space_Invaders.ino
+++ Space_Invaders.ino
@@ -1,72 +1,52 @@
-#include <SPI.h>>
+#include <SPI.h>
 #include <Gamebuino.h>
 Gamebuino gb;
-//Sorry for this pointers but I wanted to play with them a bit 
-{*/
-84x48  resolution
-
-divisors of 84	1, 2, 3, 4, 6, 7, 12, 14, 21, 28, 42, 84
-								84 42	28 21 14 12 7		6		4		3		2		1 
-divisors of 48	1, 2, 3, 4, 6, 8, 12, 16, 24, 48
-								48 24 16 12 8  6  4   3   2   1
-
-*/}
+//84x48  resolution
 
 #define TEST 1
 
-{ //BITMAPS
-const byte ship[] PROGMEM = {7, 3,
-0001000,
-0011100,
-0111110
-}
-
-const byte alien_1[] PROGMEM = {9, 9,
-000000000,
-011111110,
-010010010,
-010010010,
-010010010,
-010010010,
-010010010,
-011111110,
-000000000
-}
-const byte alien_2[] PROGMEM = {9, 9,
-000000000,
-011111110,
-010010010,
-010101010,
-010010010,
-010100010,
-010111010,
-011111110,
-000000000
-}
-const byte alien_3[] PROGMEM = {9, 9,
-000000000,
-011111110,
-010111010,
-010001010,
-010111010,
-010001010,
-010111010,
-011111110,
-000000000
-}
-}//BITMAPS END
+const byte ship[] PROGMEM = {8, 3,
+B00100000,
+B01110000,
+B11111000,
+};
+const byte alien_1[] PROGMEM = {
+8, 8,
+B11111111,
+B10010001,
+B10010001,
+B10010001,
+B10010001,
+B10010001,
+B10010001,
+B11111111,
+};
+const byte alien_2[] PROGMEM = {8, 8,
+B11111111,
+B11111111,
+B10000101,
+B10001001,
+B10010001,
+B10100001,
+B11111111,
+B11111111,
+};
+const byte alien_3[] PROGMEM = {8, 8,
+B11111111,
+B11111111,
+B10000011,
+B10011111,
+B10000011,
+B10000011,
+B11111111,
+B11111111,
+};
 
 int ship_v = 1;
 int ship_x = 38;
 const int SHIP_Y = LCDHEIGHT - 3;
 
 int alien_v = 1;
-int alien_1_x;
-int alien_1_y;
-int alien_2_x;
-int alien_2_y;
-int alien_3_x;
-int alien_3_y;
 
 bool aliens[3][8];
 int aliens_X[8] = {0, 9, 18, 27, 36, 45, 54, 63};
@@ -75,13 +55,13 @@ const int aliens_Y[3] = {1, 10, 19};
 void setup(){
 	gb.begin();
 	gb.titleScreen(F("Space_Invaders"));
+	gb.display.persistence = false;
 	
 	for (int i = 0; i < 3; i++){ //setup 3 rows of aliens ready for invasion
 		for (int j = 0; j < 8; j++){
 			aliens[i][j] = true;
 		}
 	}
-	
 }
 
 void loop(){
@@ -96,9 +76,18 @@ while (gb.update()){ //returns true every 50ms; 20fps
 	}
 
 	//LOGIC
-
+	if (*aliens_X < 12){
+		for (int i = 0; i < 8; i++){
+			*(aliens_X + i) += alien_v;
+		}
+	} else if(*aliens_X > 0){
+		for (int i = 0; i < 8; i++){
+			*(aliens_X + i) -= alien_v;
+		}
+	}
 
 	//DRAW
+	gb.display.clear();
 	gb.display.drawBitmap(ship_x, SHIP_Y, ship);
 	
 	for (int i = 0; i < 3; i++){ //Draw aliens
@@ -118,7 +107,6 @@ while (gb.update()){ //returns true every 50ms; 20fps
 			}											
 		}
 	} //Draw aliens END
-													
 													
 }
 }
